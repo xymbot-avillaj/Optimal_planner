41350e258bf05f3e441c83897719554cf0b579dc
xymbot-avillaj <alberto.villalonga@xymbot.com>
2022-12-13 15:40:10 +0100

    Changes in optimization

--- libraries/optimal_planner.py
+++ libraries/optimal_planner.py
@@ -12,27 +12,31 @@ import requests
 ORION_URL = ''
 
 Operators=[] #operadores 
-Priority=[]  #ordenes ordenadas por prioridad
+Orders=[]  #ordenes  
 ocupation={} #diccionario para saber si un operador esta ocupado
 production={} #diccionario que contiene arreglo de operadores ordenados por prudictividad por cada operador seleccionado
 product=[]
 Solution=[] # matriz que contiene los 3 operadores para cada orden 
 time=[]# numero de turnos por orden para finalizar produccion
 
+epch=200
+
 class Solve(Problem):
 
     def __init__(self, c):
-        super().__init__(n_var=np.shape(c)[0]*np.shape(c)[1], n_obj=1, n_ieq_constr=1, xl=np.zeros(np.shape(c)[0]*np.shape(c)[1]),
-        xl=np.ones(np.shape(c)[0]*np.shape(c)[1]))
+        super().__init__(n_var=np.shape(c)[0]*np.shape(c)[1], n_obj=1,n_eq_constr=np.shape(c)[1], n_ieq_constr=np.shape(c)[0], xu=1,
+        xl=0)
         self.C=c
 
     def _evaluate(self, x, out, *args, **kwargs):
         num_rows, num_cols = self.C.shape
-        out["F"] = - np.sum(np.dot(-1,np.multiply(self.C,np.reshape(x,num_rows,num_cols))))
-        g1=3-np.reshape(x,num_rows,num_cols).sum(axis=1)
-        g2=1-np.reshape(x,num_rows,num_cols).sum(axis=0)
-        G = np.column_stack([g1, g2])
-        out["G"] = G
+        x = np.reshape(x, (epch, num_rows, num_cols))
+        D = np.multiply(self.C, x)
+        out["F"] = -1 * np.sum(np.sum(D,axis=2), axis=1)
+        g1=np.sum(x, axis=1)-3
+        g2=np.sum(x, axis=2)-1
+        out['H']=g1
+        out["G"] =g2
 
 def postResult(path, data, headers={'Content-Type': 'application/json'}):
     x = requests.post(path, headers=headers, json=data)
@@ -40,9 +44,9 @@ def postResult(path, data, headers={'Content-Type': 'application/json'}):
 
 def ini(op:list,prio:list,prod:list):
     """ Function to initialize the arrays for the optimization """
-    global Priority, Operators, product
+    global Orders, Operators, product
     Operators=op
-    Priority=prio
+    Orders=prio
     product= prod
     for i in range (0,len(Operators)):
         ocupation[Operators[i]]=False
@@ -52,14 +56,14 @@ def med(op,sch):
     """Function to calculate the mean production of a operator"""
     pass
 
-def optimization_priority():
-    """Function optimization with priority"""
-    for i in range (0,len(Priority)):
+def optimization_Orders():
+    """Function optimization with Orders"""
+    for i in range (0,len(Orders)):
         sol=[]
         for j in range (0,len(Operators)):
-            if not ocupation [production[Priority[i]][j]]:
-                sol.append(production[Priority[i]][j])
-                ocupation [production[Priority[i]][j]]=True
+            if not ocupation [production[Orders[i]][j]]:
+                sol.append(production[Orders[i]][j])
+                ocupation [production[Orders[i]][j]]=True
             if len (sol) == 3:
                 break
         Solution.append[sol]
@@ -77,25 +81,25 @@ def optimization_priority():
     # send the solutions back to Orion
 
 def optimization ():
-    """Function optimization without priority"""
-    algorithm = GA(pop_size=200,sampling=BinaryRandomSampling(),crossover=TwoPointCrossover(),mutation=BitflipMutation(),
+    """Function optimization without Orders"""
+    algorithm = GA(pop_size=epch,sampling=BinaryRandomSampling(),crossover=TwoPointCrossover(),mutation=BitflipMutation(),
     eliminate_duplicates=True)
     C=np.array(product)
     solv=Solve(C)
     res = minimize(solv,algorithm,('n_gen', 200),verbose=False)
     num_rows, num_cols = C.shape
-    solut=np.reshape(res,num_rows, num_cols)
+    solut=np.reshape(res.X,num_rows, num_cols)
     Oper=[]
     Mach=[]
-    for i in range (0,len(Priority)):
+    for i in range (0,len(Orders)):
         O=[]
-        Mach.append(Priority[i])
+        Mach.append(Orders[i])
         for j in range (0,len(Operators)):
             if solut[i][j]:
                 O.append(Operators[j])
         Oper.append(O)
-    entity = {'id': 'urn:ngsi:OPS:001', 'type': 'Optimal_Planner', 'Equipment':{'value':[]} ,'Operators':{'value':[]}}
-    entity["Equipment"]['value'].append(Priority)
+    entity = {'id': 'urn:ngsi:OptimalPlanner:001', 'type': 'Optimal_Planner', 'Equipment':{'value':[]} ,'Operators':{'value':[]}}
+    entity["Equipment"]['value'].append(Orders)
     entity["Operators"]['value'].append(Oper)
     try:
             print(postResult(ORION_URL +'/v2/entities?options=upsert', entity, {'Content-Type': 'application/json',}))
